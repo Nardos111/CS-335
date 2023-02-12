dad3703db925fb77bbed7b66dd6f9b860df6764d
nardos111 <nardos2alemu@gmail.com>
2023-02-12 14:33:28 -0500

    Add create task command

---
+++ Task Manager App/src/actions/Create_Task.java
@@ -0,0 +1,40 @@
+package actions;
+import Task.Task;
+
+import java.util.Scanner;
+
+
+public class Create_Task extends Actions {
+	
+	public void displayMessage() {
+		System.out.println("To add a new task make sure you follow the following format...");
+	}
+	
+	public String userInput() {
+		while(true) {
+			System.out.println("Enter task: ");
+			try (Scanner scanner = new Scanner(System.in)) {
+				String userInput = scanner.nextLine();
+				
+				String[] inputs = userInput.split(",");
+				if (inputs.length == 7) {
+					return userInput;
+				}
+				else {
+					System.out.println("Please enter a valid input. ");
+				}
+			}
+		}
+		
+	}
+	
+	public void doAction(String action) {
+		String[] inputs = action.split(",");
+		Task task = Task.buildTask(inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5], inputs[06]);
+//		Created task needs to be added to a list here
+		
+		System.out.println("New task has been added!");
+		
+	}	
+
+}
