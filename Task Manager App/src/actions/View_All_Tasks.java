5f780c6f276fd814cb77c0b795fc86ba1faee555
nardos111 <nardos2alemu@gmail.com>
2023-02-12 15:55:42 -0500

    display all tasks

---
+++ Task Manager App/src/actions/View_All_Tasks.java
@@ -0,0 +1,18 @@
+package actions;
+
+
+public class View_All_Tasks  extends Actions{
+	
+	public void displayMessage() {
+		System.out.println("Display all tasks ");
+	}
+	
+	public String userInput() {
+		throw new UnsupportedOperationException("No input is required for this.");
+	}
+	
+	public void doAction(String action) {
+//		Display all tasks
+	}
+	
+}
