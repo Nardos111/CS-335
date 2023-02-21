6eff45977677757d5758f2d2aeb6f4a9b2c9c2bb
Samantha Margolin <113360762+margols@users.noreply.github.com>
2023-02-20 20:34:14 -0500

    Update Run_Program.java

--- Task-Manager-master/Task Manager App/src/actions/actions/Run_Program.java
+++ Task-Manager-master/Task Manager App/src/actions/actions/Run_Program.java
@@ -12,7 +12,7 @@ public class Run_Program {
         System.out.println("  "); 
         task_manager.saveToFile("/Users/samanthamargolin/Desktop/Task-Manager-master");
         System.out.println(" "); 
-        task_manager.readFile("/Users/samanthamargolin/Desktop/Task-Manager-master/Task Manager App/src/test_readfile"); 
+        //task_manager.readFile("/Users/samanthamargolin/Desktop/Task-Manager-master/Task Manager App/src/test_readfile"); 
     }
 
 }
