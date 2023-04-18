eea3b69804a17bba6564349829590ce1468a3ded
nardos111 <nardos2alemu@gmail.com>
2023-04-18 11:09:33 -0500

    Add filter options for tasks

--- Task Manager App/src/actions/Filter_Tasks.java
+++ Task Manager App/src/actions/Filter_Tasks.java
@@ -19,49 +19,106 @@ public class Filter_Tasks  extends Actions{
 	}
 	
 	public String userInput() {
-		while(true) {
-			System.out.println("Which task status do you want to filter? ");
-			System.out.println("1. To-do");
-			System.out.println("2. In-progress");
-			System.out.println("3. Completed");
-			Scanner scanner = new Scanner(System.in); 
-				String userInput = scanner.nextLine();
-				try {
-					int number = Integer.parseInt(userInput);
-				} catch(NumberFormatException nfe) {
-					System.out.println("Please enter a valid input. ");
-				}	
-				return userInput;
+		while(true ) {
+			System.out.println("What do you want to use to filter your tasks?");
+			System.out.println("1. Priority");
+			System.out.println("2. Category");
+			System.out.println("3. Status");
+			Scanner scanner = new Scanner(System.in);
+			String userChoice = scanner.nextLine();
+			try {
+				int number = Integer.parseInt(userChoice);
+			} 
+//			needs validation to check if user input is between 1-3
+			catch (NumberFormatException nfe) {
+				System.out.println("Please enter a valid input");
+			}
+			return userChoice;	
 		}
-	} 
+	}
+	
+	
 	public void doAction(String action) {
 		List<Task> filteredTasks = null;
+		Scanner scanner = new Scanner(System.in);
 		switch(action) {
 		case "1":
-			filteredTasks = Task_List.tasks.stream().filter(task -> task.getStatus().contains("To-do")).collect(Collectors.toList());
+			System.out.println("Which priority do you want to select?");
+			System.out.println("1 ");
+			System.out.println("2 ");
+			System.out.println("3 ");
+			String userInput = scanner.nextLine();
+			try {
+				int number = Integer.parseInt(userInput);
+			} 
+//				needs another validation to check if user enters a number between 1-3
+			catch(NumberFormatException nfe) {
+				System.out.println("Please enter a valid input");
+			}
+			switch(userInput) {
+			case "1":
+				filteredTasks = Task_List.tasks.stream().filter(task -> task.getPriority()==1).collect(Collectors.toList());
+				break;
+			case "2":
+				filteredTasks = Task_List.tasks.stream().filter(task -> task.getPriority()==2).collect(Collectors.toList());
+				break;
+			case "3":
+				filteredTasks = Task_List.tasks.stream().filter(task -> task.getPriority()==3).collect(Collectors.toList());
+				break;
+			}
 			break;
 		case "2":
-			filteredTasks = Task_List.tasks.stream().filter(task -> task.getStatus().contains("In-progress")).collect(Collectors.toList());
+			System.out.println("What category do you want to use to filter your tasks? ");
+			String userInput2 = scanner.nextLine();
+			filteredTasks = Task_List.tasks.stream().filter(task -> task.getCategory().contains(userInput2)).collect(Collectors.toList());
 			break;
 		case "3":
-			filteredTasks = Task_List.tasks.stream().filter(task -> task.getStatus().contains("Completed")).collect(Collectors.toList());
-			break;
+			while(true) {
+				System.out.println("Which task status do you want to filter? ");
+				System.out.println("1. To-do");
+				System.out.println("2. In-progress");
+				System.out.println("3. Completed");
+				String userInput3 = scanner.nextLine();
+				try {
+					int number = Integer.parseInt(userInput3);
+				} catch(NumberFormatException nfe) {
+						System.out.println("Please enter a valid input. ");
+				}	
+				switch(userInput3) {
+				case "1":
+					filteredTasks = Task_List.tasks.stream().filter(task -> task.getStatus().contains("To-do")).collect(Collectors.toList());
+					break;
+				case "2":
+					filteredTasks = Task_List.tasks.stream().filter(task -> task.getStatus().contains("In-progress")).collect(Collectors.toList());
+					break;
+				case "3":
+					filteredTasks = Task_List.tasks.stream().filter(task -> task.getStatus().contains("Completed")).collect(Collectors.toList());
+					break;
+				}
+				break;
+			}
 		}
-		Iterator<Task> it = filteredTasks.iterator();
-		AtomicInteger counter = new AtomicInteger(1);
-	    while(it.hasNext()){
-	    	Task task = it.next();
-	    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-			String dueDate = task.getDueDate().format(formatter);
-			System.out.println("Task "+counter.get());
-			System.out.println("Title: " + task.getTitle());
-			System.out.println("Due date: " + dueDate);
-			System.out.println("Description: " + task.getDescription());
-			System.out.println("Priority: " + task.getPriority());
-			System.out.println("Category: " + task.getCategory());
-			System.out.println("Status: " + task.getStatus());
-			System.out.println("");
-			counter.getAndIncrement();
+		if(filteredTasks.size()==0) {
+			System.out.print("There is no task with that specification");
+			System.out.println();
+		}
+		else {	
+			Iterator<Task> it = filteredTasks.iterator();
+			AtomicInteger counter = new AtomicInteger(1);
+			while(it.hasNext()){
+				Task task = it.next();
+		    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+				String dueDate = task.getDueDate().format(formatter);
+				System.out.println("Task "+counter.get());
+				System.out.println("Title: " + task.getTitle());
+				System.out.println("Due date: " + dueDate);
+				System.out.println("Description: " + task.getDescription());
+				System.out.println("Priority: " + task.getPriority());
+				System.out.println("Category: " + task.getCategory());
+				System.out.println("Status: " + task.getStatus());
+				System.out.println("");
+				counter.getAndIncrement();
+			}
 	    }
 	}
 }
